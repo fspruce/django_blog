73ec569f06cecc95ce0d0cc362626016724527d8
fspruce <f.spruce@proton.me>
2025-09-10 13:23:47 +0100

    Add blog_index url to codestar project urls

--- codestar/urls.py
+++ codestar/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from blog import index as blog_index
 
 urlpatterns = [
+    path('', blog_index, name='blog_index'),
     path('admin/', admin.site.urls),
 ]
