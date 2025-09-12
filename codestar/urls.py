827bba86439e15454ec5928f998a2fc1370a548a
fspruce <f.spruce@proton.me>
2025-09-12 14:59:15 +0100

    Remove views code, move blog urls to blog folder,  add html to templates/blog

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,9 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as blog_views
 
 urlpatterns = [
-    path('', blog_views.index, name='blog_views'),
+    path('', include("blog.urls"), name='blog_views'),
     path('admin/', admin.site.urls),
 ]
