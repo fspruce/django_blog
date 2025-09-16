1ae63539a04aa5dc1be742efc3ce9f31ab885c96
fspruce <f.spruce@proton.me>
2025-09-16 12:52:04 +0100

    Create automated tests for app views

--- about/test_forms.py
+++ about/test_forms.py
@@ -18,14 +18,21 @@ class TestCollaborateForm(TestCase):
     def test_name_is_valid(self):
         """ Test for name field """
         form = CollaborateForm({'name': ''})
-        self.assertFalse(form.is_valid(), msg="name is empty, but form still valid")
+        self.assertFalse(form.is_valid(),
+                         msg="name is empty, but form still valid"
+                         )
 
     def test_email_is_valid(self):
         """ Test for email field """
         form = CollaborateForm({'email': ''})
-        self.assertFalse(form.is_valid(), msg="email is empty, but form still valid")
+        self.assertFalse(form.is_valid(),
+                         msg="email is empty, but form still valid"
+                         )
 
     def test_message_is_valid(self):
         """ Test for message field """
         form = CollaborateForm({'message': ''})
-        self.assertFalse(form.is_valid(), msg="message is empty, but form still valid")
+        self.assertFalse(
+            form.is_valid(),
+            msg="message is empty, but form still valid"
+        )
